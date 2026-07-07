5ca83014f13ee428a97adc01777a831ea8947064
baiyshbaike <baiyshbeksamatov@gmail.com>
2026-07-07 21:47:07 +0600

    database healthy

--- backend/src/Dialysis.Api/Program.cs
+++ backend/src/Dialysis.Api/Program.cs
@@ -3,12 +3,12 @@ using Dialysis.Infrastructure;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+var connectionString = builder.Configuration.GetConnectionString("Default");
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddInfrastructure(builder.Configuration);
-builder.Services.AddHealthChecks();
+builder.Services.AddHealthChecks().AddNpgSql(connectionString!);
 //builder.Services.AddOpenApi();
 
 var app = builder.Build();
