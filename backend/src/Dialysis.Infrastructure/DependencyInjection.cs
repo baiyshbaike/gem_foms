e638ae0ba60116e5dfe86062483454169bf1620e
baiyshbaike <baiyshbeksamatov@gmail.com>
2026-07-07 20:53:42 +0600

    chore: add empty application db context

---
+++ backend/src/Dialysis.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,16 @@
+﻿using Dialysis.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Dialysis.Infrastructure;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+    {
+        var conncetionsString = configuration.GetConnectionString("Default");
+        services.AddDbContext<AppDbContext>(options => options.UseNpgsql(conncetionsString));
+        return services;
+    }
+}
